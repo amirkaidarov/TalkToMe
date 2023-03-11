2215d6a55d68b229283f3d46aa89b0218044ec0b
amirkaidarov <KaidarovAmir1441@gmail.com>
2023-03-11 14:35:41 -0500

    Initial Commit

---
+++ TalkToMe/TalkToMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkToMeApp.swift
+//  TalkToMe
+//
+//  Created by Амир Кайдаров on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TalkToMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
